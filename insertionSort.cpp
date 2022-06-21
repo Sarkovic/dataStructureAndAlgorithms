01a10cb57276ccb90af20ecf172ceb13b05d7855
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2022-06-22 03:39:26 +0600

    Initial commit

---
+++ insertionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a[] = {10, 5, 8, 7};
+    int N = sizeof(a) / sizeof(int);
+
+    for (int j = 1; j < N; j++)
+    {
+        int i = j - 1;
+        int key = a[j];
+
+        while (i >= 0 && a[i] < key)
+        {
+            a[i + 1] = a[i];
+            i = i - 1;
+        }
+        a[i+1] = key;
+    }
+
+    for(int i = 0; i < N; i++)
+        cout << a[i] << endl;
+    
+
+    return 0;
+}
