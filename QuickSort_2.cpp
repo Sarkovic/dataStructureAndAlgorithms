d36ed75c105c5dd55348aedb736e70ba23cf4500
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2022-07-25 22:25:42 +0600

    Edited

--- QuickSort_2.cpp
+++ QuickSort_2.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-int partioning(int array[], int low, int high){
+int partition(int array[], int low, int high){
     int pivot = array[high];
     int i = low - 1;
 
-    for (int j = low; j <= high - 1; j++)
+    for (int j = low; j < high; j++)
     {
-        if(array[j] < pivot){
+        if (array[j] < pivot)
+        {
             i++;
             swap(array[i], array[j]);
         }
@@ -17,10 +18,11 @@ int partioning(int array[], int low, int high){
     return i+1;
 }
 
-void quickSort(int array[], int low, int high){
-    if(low < high){
-        int pi = partioning(array, low, high);
-        quickSort(array, low, pi - 1);
+void quickSort(int array[], int low, int high) {
+    if (low < high)
+    {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi-1);
         quickSort(array, pi+1, high);
     }
 }
