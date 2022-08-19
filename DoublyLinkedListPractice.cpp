4a58c68c11fb11ce34917c0c3a054f5af689dc5c
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2022-08-19 16:07:15 +0600

    Incomplete

---
+++ DoublyLinkedListPractice.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+typedef struct Node node;
+
+node* head = NULL;
+node* tail = NULL;
+
+node* createNode(int value) {
+    node* temp = (node *)malloc(sizeof(node));
+    temp -> data = value;
+    temp -> prev = NULL;
+    temp -> next = NULL;
+
+    return temp;
+}
+
+void append(int value){
+    if(head == NULL){
+        head = createNode(value);
+        tail = head;
+    }
+    else{
+        node* temp = createNode(value);
+        tail -> next = temp;
+        tail = tail -> next;
+    }
+}
+
+node* searchNode(int value){
+    for (node* i = head; i != NULL; i = i -> next)
+    {
+        if(i -> data == value){
+            return i;
+        }
+    }
+}
+
+void insertFirst(int value){
+    Node* temp = createNode(value);
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+    head -> prev = temp;
+    temp -> next = head;
+    head = temp;
+}
+
+void insertLast(int value){
+    Node* newNode = createNode(value);
+    Node* temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = newNode;
+    newNode -> prev = temp;
+}
+
+void insertAfterCertainData(int insertAfter, int value){
+    node* newNode = createNode(value);
+    node* temp = searchNode(insertAfter);
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+}
+
+void printList(){
+    node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+void deleteFirst(){
+    node* temp = head;
+    head = head -> next;
+    free(temp);
+}
+
+void deleteLast(){
+    node* temp;
+
+    if(head == NULL){
+        cout << "\n Underflow" << endl;
+    }
+    else if(head -> next == NULL){
+        head = NULL;
+        free(head);
+        cout << "\n Node Deleted" << endl;
+    }
+    else{
+        temp = head;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->prev->next = NULL;
+        free(temp);
+        cout << "\nNode Deleted" << endl;
+    }
+}
+
+int main() {
+    for (int i = 10; i <= 50; i+=10)
+    {
+        append(i);
+    }
+
+    printList();
+
+    insertLast(33464);
+    printList();
+
+    deleteLast();
+    printList();
+    
+    return 0;
+}
